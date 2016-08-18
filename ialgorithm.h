b3c22f8f9e3a3b9685568b14521bed42f9cea438
maked0n <alex.mikhalevich@gmail.com>
2016-08-18 12:10:08 +0300

    Qt plugins support

--- ialgorithm.h
+++ ialgorithm.h
@@ -8,8 +8,12 @@ template<class CompareFunction>
 class IAlgorithm {
 public:
        virtual void apply_algorithm(const std::vector<std::vector<int> >& edges,
-                                   const std::size_t edges_size, CompareFunction func, std::istream& stream) = 0;
+                                   const std::size_t edges_size, CompareFunction func, std::istream& stream) = 0;             //TODO: probably all function params are unnecessary
+       virtual void step() = 0;
+       virtual ~IAlgorithm() {}
 };
+
+Q_DECLARE_INTERFACE(IAlgorithm, "com.maked0n.GVis.IAlgorithm")
 
 #endif // IALGORITHM
 
