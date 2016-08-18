6dd8db4e50ed09153a617697113b9005ef1f09a1
maked0n <alex.mikhalevich@gmail.com>
2016-08-18 12:07:30 +0300

    Exception class

--- cexception.hpp
+++ cexception.hpp
@@ -1,13 +1,42 @@
 #include <iostream>
+#include <string>
 
 class IException {
 public:
-       virtual void what() = 0;
+       virtual std::string what() = 0;
 };
 
 class ExInvalidInput : IException {
 public:
-       void what() {
-              std::cerr << "Input data is invalid" << std::endl;
-       }
+       std::string what() { return "Input data is invalid."; }
+};
+
+class ExLastStep : IException {
+public:
+       std::string what() { return "This is already the last step."; }
+};
+
+class ExFirstStep : IException {
+public:
+       std::string what() { return "This is already the first step."; }
+};
+
+class ExExistingVertex : IException {
+public:
+       std::string what() { return "Vertex with this id is already exists."; }
+};
+
+class ExExistingEdge : IException {
+public:
+       std::string what() { return "Edge with this id is already exists."; }
+};
+
+class ExInvalidId : IException {
+public:
+       std::string what() { return "Invalid id."; }
+};
+
+class ExInvalidVertex : IException {
+public:
+       std::string what() { return "No such vertex."; }
 };
