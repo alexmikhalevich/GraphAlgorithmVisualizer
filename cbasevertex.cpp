7bf8c3f14af85164596898f36e228d6116a07c3f
maked0n <alex.mikhalevich@gmail.com>
2016-08-19 12:37:55 +0300

    Removed unnecessary files

--- cbasevertex.cpp
+++
@@ -1,9 +0,0 @@
-#include "cbasevertex.h"
-
-void CBaseVertex::setIndex(std::size_t index) {
-       bv_vertexIndex = index;
-}
-
-std::size_t CBaseVertex::getIndex() const {
-       return bv_vertexIndex;
-}
